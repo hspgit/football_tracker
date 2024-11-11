0075899b9f82cdfef1f74e5e3fc5c3e31500ee13
Hrishikesh Pradhan <hrishikesh@Hrishikeshs-MacBook-Pro.local>
2024-11-11 16:43:12 -0500

    Removed password from config, using env variables now

--- server/config.py
+++ server/config.py
@@ -1,7 +1,11 @@
-# server/config.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'hrishikesh'
-    MYSQL_DB = 'football_tracker'
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'football_tracker')
